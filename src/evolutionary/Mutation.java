4b56de99d07f8e53842440bc4d5903338c285309
HaydenFaulkner <hayyah77@gmail.com>
2014-08-15 15:55:40 +0930

    made mutation type static

--- src/evolutionary/Mutation.java
+++ src/evolutionary/Mutation.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Random;
 
 public class Mutation {
-	private MutationType mutationType;
+	private static MutationType mutationType;
 	private static Random rand = new Random(System.currentTimeMillis());
 	
 	public enum MutationType{
