980b374012a1ea65ca9eef3032b0dae1a7fe504a
HaydenFaulkner <hayyah77@gmail.com>
2014-08-22 17:16:14 +0930

    Attempt 3

--- src/evolutionary/Mutation.java
+++ src/evolutionary/Mutation.java
@@ -210,7 +210,7 @@ public class Mutation {
 					indexA = indexB;
 					indexB = tmp;
 				}
-				int swaps = (int) (Math.floor(indexB-indexA+1)/2);//how many swap operations
+				int swaps = (int) (Math.floor(indexB-indexA)/2);//how many swap operations
 				
 				for (int j = 0; j < swaps; j++) {
 					Object temp = i.genotype.get(indexA+j);//store temp
